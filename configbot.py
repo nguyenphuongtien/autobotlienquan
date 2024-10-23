2bb9585f30235e082347b8b20eed59c359303da9
NguyenPhuongTien <nphtien.tgg@vnpt.vn>
2024-10-24 00:17:41 +0700

    Commit update 6

--- configbot.py
+++ configbot.py
@@ -4,46 +4,20 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random
 import os
 
-# Initialize logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-# File to store player information
 PLAYER_FILE = 'list_players.txt'
-
-# File to store list of players by week
 PLAYER_WEEK_FILE = 'list_players_week.txt'
+PLAYER_SOTRAN_FILE = 'list_players_sotran.txt'
+PLAYER_DIEM_FILE = 'list_players_diem.txt'
 
-async def showlistweek(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        if not os.path.exists(PLAYER_WEEK_FILE):
-            await update.message.reply_text("No players registered for the week.")
-            return
-
-        players = []
-        with open(PLAYER_WEEK_FILE, 'r') as file:
-            for line in file:
-                ingame_name, rank = line.strip().split(',')
-                players.append((ingame_name, rank))
-
-        # Sort players by rank
-        players.sort(key=lambda x: x[1])
-
-        response = "List of Players for the Week:\n"
-        for ingame_name, rank in players:
-            response += f"{ingame_name} - {rank}\n"
-
-        await update.message.reply_text(response)
-    except Exception as e:
-        logging.error(f"Error in showlistweek command: {e}")
-        await update.message.reply_text("An error occurred while showing the weekly list.")
-
-async def showlist(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def getlistall(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         if not os.path.exists(PLAYER_FILE):
-            await update.message.reply_text("No players registered.")
+            await update.message.reply_text("Không có người chơi đăng ký!")
             return
 
         players = []
@@ -52,34 +26,56 @@ async def showlist(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 ingame_name, rank = line.strip().split(',')
                 players.append((ingame_name, rank))
 
-        # Sort players by rank
-        players.sort(key=lambda x: (x[1], x[0]))
-
-        response = "List of Players:\n"
+        players.sort(key=lambda x: (x[1]), reverse=True)
+        stt = 0
+        response = "Danh sách người chơi đã đăng ký:\n"
         for ingame_name, rank in players:
-            response += f"{ingame_name} - {rank}\n"
+            stt = stt + 1
+            response += f"{stt}. {rank} {ingame_name}\n"
 
         await update.message.reply_text(response)
     except Exception as e:
-        logging.error(f"Error in showlist command: {e}")
-        await update.message.reply_text("An error occurred while showing the list.")
+        logging.error(f"Lỗi trong quá trình getlist: {e}")
+        await update.message.reply_text("Lỗi trong quá trình getlist.")
 
-#register a player
+async def getlistofweek(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        if not os.path.exists(PLAYER_WEEK_FILE):
+            await update.message.reply_text("Không có người chơi đăng ký trong tuần này!")
+            return
+
+        players = []
+        with open(PLAYER_WEEK_FILE, 'r') as file:
+            for line in file:
+                ingame_name, rank, sotran = line.strip().split(',')
+                players.append((ingame_name, rank, sotran))
+
+        players.sort(key=lambda x: x[2], reverse=True)
+        stt = 0
+        response = "Danh sách người chơi đăng ký trong tuần này:\n"
+        for ingame_name, rank, sotran in players:
+            stt = stt + 1
+            response += f"{stt}. {rank} {ingame_name} số trận: {sotran}\n"
+
+        await update.message.reply_text(response)
+    except Exception as e:
+        logging.error(f"Lỗi trong quá trình getlistofweek: {e}")
+        await update.message.reply_text("Lỗi trong quá trình getlistofweek!")
+
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         args = context.args
         if len(args) != 2:
-            await update.message.reply_text("Sai cú pháp: /register <IngameName> <Rank>")
+            await update.message.reply_text("Sai cú pháp: /register <ingamename> <rank>")
             return
 
         ingame_name = args[0]
         rank = args[1]
 
-        if rank not in ['R1', 'R2', 'R3']:
-            await update.message.reply_text("Rank không đúng phải là một trong ba giá trị: R1, R2, R3!")
+        if rank not in ['R1', 'R2', 'R3', 'R4', 'R5']:
+            await update.message.reply_text("Sai cú pháp rank là một trong các giá trị: R1, R2, R3, R4, R5")
             return
 
-        # Check if the player already exists
         if os.path.exists(PLAYER_FILE):
             with open(PLAYER_FILE, 'r') as file:
                 for line in file:
@@ -87,27 +83,44 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
                         await update.message.reply_text(f"Người chơi {ingame_name} đã được đăng ký!")
                         return
 
-        # Save the player information to the file
-        with open(PLAYER_FILE, 'a') as file:
-            file.write(f"\n{ingame_name},{rank}")
+        with open(PLAYER_FILE, 'a+') as file:
+            file.seek(0, os.SEEK_END)
+            if file.tell() > 0:
+                file.write(f"\n{ingame_name},{rank}")
+            else:
+                file.write(f"{ingame_name},{rank}")
 
-        await update.message.reply_text(f"Chào mừng {ingame_name} với rank {rank} đã đăng ký thành công!")
+        with open(PLAYER_SOTRAN_FILE, 'a+') as file:
+            file.seek(0, os.SEEK_END)
+            if file.tell() > 0:
+                file.write(f"\n{ingame_name},0")
+            else:
+                file.write(f"{ingame_name},0")
+
+        with open(PLAYER_DIEM_FILE, 'a+') as file:
+            file.seek(0, os.SEEK_END)
+            if file.tell() > 0:
+                file.write(f"\n{ingame_name},0")
+            else:
+                file.write(f"{ingame_name},0")
+
+        await update.message.reply_text(f"Chào mừng {ingame_name} {rank} đã đăng ký thành công!")
     except Exception as e:
-        logging.error(f"Error in register command: {e}")
-        await update.message.reply_text("An error occurred during registration.")
+        logging.error(f"Lỗi trong register: {e}")
+        await update.message.reply_text("Lỗi không thể đăng ký!")
 
 async def registerweek(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         args = context.args
         if len(args) != 2:
-            await update.message.reply_text("Sai cú pháp: /registerweek <IngameName> <Rank>")
+            await update.message.reply_text("Sai cú pháp: /registerweek <ingamename> <rank>")
             return
 
         ingame_name = args[0]
         rank = args[1]
 
-        if rank not in ['R1', 'R2', 'R3']:
-            await update.message.reply_text("Rank không đúng phải là một trong ba giá trị: R1, R2, R3")
+        if rank not in ['R1', 'R2', 'R3', 'R4', 'R5']:
+            await update.message.reply_text("Sai cú pháp rank là một trong các giá trị: R1, R2, R3, R4, R5")
             return
 
         player_exists = False
@@ -133,110 +146,84 @@ async def registerweek(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_text(f"Người chơi {ingame_name} đã được đăng ký cho tuần này!")
             return
 
-        # Save the player information to the weekly file
+        sotran = 0
+        if os.path.exists(PLAYER_SOTRAN_FILE):
+            with open(PLAYER_FILE, 'r') as file:
+                for line in file:
+                    name, sotrandata = line.strip().split(',')
+                    if name == ingame_name:
+                        sotran = int(sotrandata)
+
         with open(PLAYER_WEEK_FILE, 'a') as file:
-            file.write(f"\n{ingame_name},{rank}")
+            file.write(f"\n{ingame_name},{rank},{sotran}")
 
-        await update.message.reply_text(f"Chào mừng {ingame_name} với rank {rank} đã đăng ký tuần này thành công!")
+        await update.message.reply_text(f"Chào mừng {ingame_name} {rank} đã đăng ký tuần này thành công!")
     except Exception as e:
-        logging.error(f"Error in registerweek command: {e}")
-        await update.message.reply_text("An error occurred during weekly registration.")
+        logging.error(f"Lỗi registerweek command: {e}")
+        await update.message.reply_text("Lỗi không thể đăng ký!.")
 
-# Random teams
 async def random_teams(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         if not os.path.exists(PLAYER_WEEK_FILE):
-            await update.message.reply_text("No players registered for the week.")
+            await update.message.reply_text("Chưa có người chơi đăng ký trong tuần này!")
             return
 
-        r1_players = []
-        r2_players = []
-        r3_players = []
+        players = []
 
         with open(PLAYER_WEEK_FILE, 'r') as file:
             for line in file:
-                try:
-                    ingame_name, rank = line.strip().split(',')
-                    if rank == 'R1':
-                        r1_players.append(ingame_name)
-                    elif rank == 'R2':
-                        r2_players.append(ingame_name)
-                    elif rank == 'R3':
-                        r3_players.append(ingame_name)
-                except ValueError:
-                    break
+                ingame_name, rank, sotran = line.strip().split(',')
+                players.append((ingame_name, rank, int(sotran)))
+
+        if len(players) < 10:
+            await update.message.reply_text("Số người chơi dưới 10 người không thể random team!")
+            return
+
+        # sort theo số trận
+        players.sort(key=lambda x: x[2], reverse=True)
+        players = players[-10:]
+
+        r1_players = [p for p in players if p[1] == 'R1']
+        r2_players = [p for p in players if p[1] == 'R2']
+        r3_players = [p for p in players if p[1] == 'R3']
+        r4_players = [p for p in players if p[1] == 'R4']
+        r5_players = [p for p in players if p[1] == 'R5']
 
         random.shuffle(r1_players)
         random.shuffle(r2_players)
         random.shuffle(r3_players)
+        random.shuffle(r4_players)
+        random.shuffle(r5_players)
 
-        total_players = len(r1_players) + len(r2_players) + len(r3_players)
-        if total_players < 10:
-            await update.message.reply_text("Số người chơi dưới 10 người không thể random team!")
-            return
+        teams = [[], []]
+        team_points = [0, 0]
 
-        if total_players < 15:
-            num_teams = 2
-        elif total_players < 20:
-            num_teams = 3
-        elif total_players < 25:
-            num_teams = 4
-        elif total_players < 30:
-            num_teams = 5
-        else:
-            await update.message.reply_text("chưa được cấu hình lớn hơn 5 đội chơi!")
-            return
+        def add_player_to_team(player, team_index):
+            teams[team_index].append(player[0])
+            team_points[team_index] += int(player[1][1])
 
-        teams = [[] for _ in range(num_teams)]
-        team_points = [0] * num_teams
+        while r1_players or r2_players or r3_players or r4_players or r5_players:
+            for i in range(2):
+                if r1_players:
+                    add_player_to_team(r1_players.pop(), i)
+                elif r2_players:
+                    add_player_to_team(r2_players.pop(), i)
+                elif r3_players:
+                    add_player_to_team(r3_players.pop(), i)
+                elif r4_players:
+                    add_player_to_team(r4_players.pop(), i)
+                elif r5_players:
+                    add_player_to_team(r5_players.pop(), i)
 
-        total_points = len(r1_players) * 3 + len(r2_players) * 2 + len(r3_players)
-        average_points_per_player = total_points / total_players
-
-        while r1_players or r2_players or r3_players:
-            for i in range(1):
-                player_picked = False
-                while not player_picked:
-                    try:
-                        rankRandom = ['R1', 'R2', 'R3']
-                        choice = random.choice(rankRandom)
-                        if not r1_players:
-                            choice = random.choice(rankRandom.remove('R1'))
-
-                        if not r2_players:
-                            choice = random.choice(rankRandom.remove('R2'))
-
-                        if not r2_players:
-                            choice = random.choice(rankRandom.remove('R3'))
-
-                        if choice == 'R1' and r1_players:
-                            player = r1_players.pop()
-                            teams[i].append(player)
-                            team_points[i] += 3
-                        elif choice == 'R2' and r2_players:
-                            player = r2_players.pop()
-                            teams[i].append(player)
-                            team_points[i] += 2
-                        elif choice == 'R3' and r3_players:
-                            player = r3_players.pop()
-                            teams[i].append(player)
-                            team_points[i] += 1
-
-                        if (len(teams[i]) == 5):
-                            player_picked = True
-
-                    except IndexError:
-                        break
-
-        response = "Randomized Teams:\n"
+        response = "Teams đã random:\n"
         for i, team in enumerate(teams):
-            response += f"Team {i + 1}:\n"
+            response += f"Team {i + 1} (Points: {team_points[i]}):\n"
             for player in team:
                 response += f"  - {player}\n"
         await update.message.reply_text(response)
     except Exception as e:
         logging.error(f"Error in random_teams command: {e}")
-        await update.message.reply_text("An error occurred during team randomization.")
+        await update.message.reply_text("Lỗi không thể random")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7988356940:AAGG13Q_EUHxPZJTE6WoYBn2YBX1lLgK2K0').build()
@@ -247,8 +234,8 @@ if __name__ == '__main__':
 
     application.add_handler(CommandHandler('random', random_teams))
 
-    application.add_handler(CommandHandler('showlist', showlist))
+    application.add_handler(CommandHandler('getlistall', getlistall))
 
-    application.add_handler(CommandHandler('showlistweek', showlistweek))
+    application.add_handler(CommandHandler('getlistofweek', getlistofweek))
 
     application.run_polling()
